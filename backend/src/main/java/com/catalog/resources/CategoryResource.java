e9e0d8e0e9014360ba486711409093d5aa579a41
iivanabm <ivanammarinho@hotmail.com>
2021-09-04 16:27:06 -0300

    Find category by id

--- backend/src/main/java/com/catalog/resources/CategoryResource.java
+++ backend/src/main/java/com/catalog/resources/CategoryResource.java
@@ -6,6 +6,7 @@ import com.catalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,12 @@ public class CategoryResource {
     public ResponseEntity<List<CategoryDTO>> findAll(){
         List<CategoryDTO> list = categoryService.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
+        CategoryDTO dto = categoryService.findById(id);
+        return ResponseEntity.ok().body(dto);
     }
 
 }
--- backend/src/main/java/com/catalog/services/CategoryService.java
+++ backend/src/main/java/com/catalog/services/CategoryService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -21,5 +22,12 @@ public class CategoryService {
        List<Category> list = categoryRepository.findAll();
        return list.stream().map(eachElement -> new CategoryDTO(eachElement))
                .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> obj = categoryRepository.findById(id);
+        Category entity = obj.get();
+        return new CategoryDTO(entity);
     }
 }
