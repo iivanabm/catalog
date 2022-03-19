396c105b0fb751fc63046b7a8601dd967708a589
iivanabm <ivanammarinho@hotmail.com>
2022-03-19 14:40:23 -0300

    Add home component

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import './styles.scss';
+import { ReactComponent as MainImage } from '../../core/assets/images/main-image.svg';
 
 const Home = () => (
-  <div>
-    <h1>Home</h1>
+  <div className="home-container">
+    <div className="row home-content">
+      <div className="col-6">
+        <h1 className="text-title">
+          The best products catalog
+        </h1>
+        <p className="text-subtitle">
+          We help you to find the best products online.
+        </p>
+      </div>
+      <div className="col-6">
+        <MainImage className="main-image" />
+      </div>
+    </div>
   </div>
 );
 
